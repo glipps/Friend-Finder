d414712a03882d4ee10ce99eb655af0798729679
glipps <glipps@kent.edu>
2019-04-25 23:02:56 -0400

    Create friends.js

---
+++ friends.js
@@ -0,0 +1,53 @@
+var friendsData = [
+    {
+        name: "Finn the Human",
+        photo: "https://vignette.wikia.nocookie.net/adventuretimewithfinnandjake/images/c/cf/Finn.png/revision/latest?cb=20121022153101",
+        scores: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,]
+    },
+    {
+        name: "Jake the Dog",
+        photo: "https://vignette.wikia.nocookie.net/vsbattles/images/5/5c/Jake_the_dog.png/revision/latest?cb=20140602235755",
+        scores: [2, 3, 2, 3, 2, 3, 2, 3, 2, 3]
+    },
+    {
+        name: "Marceline the Vampire Queen",
+        photo: "https://banner2.kisspng.com/20180410/kiw/kisspng-marceline-the-vampire-queen-princess-bubblegum-ice-vampire-5acce4984f9524.881870541523377304326.jpg",
+        scores: [5, 6, 2, 2, 1, 4, 2, 9, 9, 1]
+    },
+    {
+        name: "Princess Bubblegum",
+        photo: "https://i.ebayimg.com/images/g/IPMAAOSw0JpV5cOZ/s-l640.jpg",
+        scores: [2, 4, 6, 8, 8, 6, 4, 2, 2, 4]
+    },
+    {
+        name: "Beemo",
+        photo: "https://bmobot.xyz/img/bmo.png",
+        scores: [1, 1, 2, 3, 3, 4, 5, 5, 6, 7]
+    },
+    {
+        name: "Ice King",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/7/78/Ice_King.png/220px-Ice_King.png",
+        scores: [8, 1, 7, 3, 3, 2, 8, 9, 1, 1]
+    },
+    {
+        name: "Lumpy Space Princess",
+        photo: "https://i.pinimg.com/236x/cc/83/81/cc8381bea3783c86df7268d7b8cb2e96--lumpy-space-princess-adventure-time.jpg",
+        scores: [0, 9, 5, 6, 6, 7, 2, 1, 0, 0]
+    },
+  {
+        name: "Flame Princess",
+        photo: "https://vignette.wikia.nocookie.net/adventuretimewithfinnandjake/images/3/3e/FlamePrincess.png/revision/latest?cb=20160314152748",
+        scores: [1, 1, 2, 3, 3, 4, 5, 5, 6, 7]
+    },
+    {
+        name: "Peppermint Butler",
+        photo: "https://c1.neweggimages.com/ProductImage/32-491-041-08.jpg",
+        scores: [8, 1, 7, 3, 3, 2, 8, 9, 1, 1]
+    },
+    {
+        name: "Cinnamon Bun",
+        photo: "https://www.google.com/search?q=cinnamon+bun+adventure+time&rlz=1C1CHBF_en&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjB38nZwOHhAhWBTN8KHShYD34Q_AUIDigB&biw=1271&bih=841#imgrc=T-dXO3acJ0xIDM:",
+        scores: [0, 9, 5, 6, 6, 7, 2, 1, 0, 0]
+    }
+];
+module.exports = friendsData;
